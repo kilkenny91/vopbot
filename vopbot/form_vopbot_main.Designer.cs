aff0ba68e2b43211d6cf25b759fd1665a5c50ad4
philip <philip@diouri.de>
2022-07-16 21:11:54 +0200

    working example

--- vopbot/form_vopbot_main.Designer.cs
+++ vopbot/form_vopbot_main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -35,10 +36,12 @@
             this.crawlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.reloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openDBLocationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.installToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -67,7 +70,7 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -94,12 +97,29 @@
             this.reloadToolStripMenuItem.Name = "reloadToolStripMenuItem";
             this.reloadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.reloadToolStripMenuItem.Text = "Reload";
+            this.reloadToolStripMenuItem.Click += new System.EventHandler(this.reloadToolStripMenuItem_Click);
             // 
             // openDBLocationToolStripMenuItem
             // 
             this.openDBLocationToolStripMenuItem.Name = "openDBLocationToolStripMenuItem";
             this.openDBLocationToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openDBLocationToolStripMenuItem.Text = "Open DB Location";
+            this.openDBLocationToolStripMenuItem.Click += new System.EventHandler(this.openDBLocationToolStripMenuItem_Click);
+            // 
+            // adminToolStripMenuItem
+            // 
+            this.adminToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.installToolStripMenuItem});
+            this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.adminToolStripMenuItem.Text = "Admin";
+            // 
+            // installToolStripMenuItem
+            // 
+            this.installToolStripMenuItem.Name = "installToolStripMenuItem";
+            this.installToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.installToolStripMenuItem.Text = "Install";
+            this.installToolStripMenuItem.Click += new System.EventHandler(this.installToolStripMenuItem_Click);
             // 
             // helpToolStripMenuItem
             // 
@@ -113,39 +133,55 @@
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(0, 27);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(800, 411);
+            this.dataGridView1.Size = new System.Drawing.Size(800, 386);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellLeave);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
             // 
-            // adminToolStripMenuItem
+            // label1
             // 
-            this.adminToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.installToolStripMenuItem});
-            this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
-            this.adminToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.adminToolStripMenuItem.Text = "Admin";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(599, 423);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
-            // installToolStripMenuItem
+            // button1
             // 
-            this.installToolStripMenuItem.Name = "installToolStripMenuItem";
-            this.installToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.installToolStripMenuItem.Text = "Install";
-            this.installToolStripMenuItem.Click += new System.EventHandler(this.installToolStripMenuItem_Click);
+            this.button1.Location = new System.Drawing.Point(702, 419);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // form_vop_bot_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "form_vop_bot_main";
             this.Text = "VOPbot";
             this.Load += new System.EventHandler(this.form_vop_bot_main_Load);
+            this.Shown += new System.EventHandler(this.form_vop_bot_main_Shown);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -167,5 +203,7 @@
         private DataGridView dataGridView1;
         private ToolStripMenuItem adminToolStripMenuItem;
         private ToolStripMenuItem installToolStripMenuItem;
+        private Label label1;
+        private Button button1;
     }
 }
--- vopbot/form_vopbot_main.cs
+++ vopbot/form_vopbot_main.cs
@@ -1,6 +1,6 @@
 using System.Data;
 using System.Data.SQLite;
-
+using System.Diagnostics;
 
 namespace vopbot
 {
@@ -14,43 +14,50 @@ namespace vopbot
         private void form_vop_bot_main_Load(object sender, EventArgs e)
         {
             // dataGridView1.AutoGenerateColumns = false;
+
             string cs = @"URI=file:test.db";
             using var con = new SQLiteConnection(cs);
             con.Open();
             try
             {
-                string stm = "SELECT * FROM cars LIMIT 5";
+                string stm = "SELECT * FROM vmBackupTagsAck LIMIT 5";
                 using var selectcmd = new SQLiteCommand(stm, con);
-                using SQLiteDataReader rdr = selectcmd.ExecuteReader();
 
-                while (rdr.Read())
+                using (var reader = selectcmd.ExecuteReader())
                 {
-                    Console.WriteLine($"{rdr.GetInt32(0)} {rdr.GetString(1)} {rdr.GetInt32(2)}");
+                    string test = "";
+
+                    // do we have any data to read?
+                    //DONE: try not building string but using formatting (or string interpolation)
+                    var table = new DataTable();
+                    table.Columns.Add("VM-Name", typeof(string));
+                    table.Columns.Add("Tenant", typeof(string));
+                    table.Columns.Add("Acknowledged", typeof(string));
+                    while (reader.Read())
+                    {
+                        table.Rows.Add(reader["vmname"], reader["tenant"], reader["ack"]);
+                    }
+                    var column = new DataGridViewComboBoxColumn();
+                    column.HeaderText = "Acknowledge";
+                    column.DataPropertyName = "Acknowledge";
+                    column.DataSource = new List<string>() { "yes", "no", "unknown" };
+                    dataGridView1.DataSource = table;
+                    dataGridView1.Columns.Add(column);
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                    {
+                        dataGridView1.Rows[i].Cells[3].Value = dataGridView1.Rows[i].Cells[2].Value;
+                    }
+                    dataGridView1.Columns[0].ReadOnly = true;
+                    dataGridView1.Columns[1].ReadOnly = true;
                 }
             }
-            catch { 
-                MessageBox.Show("Table not initilized"); 
-            } 
-            finally { 
-                con.Close(); 
+            catch {
+                MessageBox.Show("Table not initilized");
             }
-            
-            var table = new DataTable();
-            table.Columns.Add("VM-Name", typeof(string));
-            table.Columns.Add("Tenant", typeof(string));
+            finally {
 
-            table.Rows.Add("Hi", "100");
-            table.Rows.Add("Ki", "30");
-
-            var column = new DataGridViewComboBoxColumn();
-            column.HeaderText = "Acknowledge";
-            column.DataPropertyName = "Acknowledge";
-            column.DataSource = new List<string>() { "yes", "no", "unknown" };
-            dataGridView1.DataSource = table;
-            dataGridView1.Columns.Add(column);
-
-            dataGridView1.Columns[0].ReadOnly = true;
-            dataGridView1.Columns[1].ReadOnly = true;
+                con.Close();
+            }
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -80,9 +87,127 @@ namespace vopbot
             con.Open();
             using var cmd = new SQLiteCommand(con);
 
-            cmd.CommandText = @"CREATE TABLE cars(id INTEGER PRIMARY KEY,
-            name TEXT, price INT)";
+            cmd.CommandText = @"CREATE TABLE vmBackupTagsAck(id INTEGER PRIMARY KEY,
+            vmname TEXT, tenant TEXT, ack TEXT)";
             cmd.ExecuteNonQuery();
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            label1.Text = "unsaved rows";
+        }
+
+        private void dataGridView1_CellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+
+        private void form_vop_bot_main_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cs = @"URI=file:test.db";
+            using var con = new SQLiteConnection(cs);
+            con.Open();
+            using var cmd = new SQLiteCommand(con);
+
+            cmd.CommandText = @"DROP TABLE vmBackupTagsAck";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"CREATE TABLE vmBackupTagsAck(id INTEGER PRIMARY KEY,
+            vmname TEXT, tenant TEXT, ack TEXT)";
+            cmd.ExecuteNonQuery();
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                try
+                {
+
+                    cmd.CommandText = @"INSERT INTO vmBackupTagsAck(vmname,tenant,ack) values ('" +
+                    dataGridView1.Rows[i].Cells[0].Value.ToString() + "','" +
+                    dataGridView1.Rows[i].Cells[1].Value.ToString() + "','" +
+                    dataGridView1.Rows[i].Cells[3].Value.ToString() + "')";
+                    cmd.ExecuteNonQuery();
+
+                }
+                catch { }
+                finally {
+                    label1.Text = "Rows saved";
+                    reload();
+                }
+            }
+        }
+        private void reload()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Refresh();
+
+            string cs = @"URI=file:test.db";
+            using var con = new SQLiteConnection(cs);
+            con.Open();
+            try
+            {
+                string stm = "SELECT * FROM vmBackupTagsAck LIMIT 5";
+                using var selectcmd = new SQLiteCommand(stm, con);
+
+                using (var reader = selectcmd.ExecuteReader())
+                {
+                    string test = "";
+
+                    // do we have any data to read?
+                    //DONE: try not building string but using formatting (or string interpolation)
+                    var table = new DataTable();
+                    table.Columns.Add("VM-Name", typeof(string));
+                    table.Columns.Add("Tenant", typeof(string));
+                    table.Columns.Add("Acknowledged", typeof(string));
+                    while (reader.Read())
+                    {
+                        table.Rows.Add(reader["vmname"], reader["tenant"], reader["ack"]);
+                    }
+                    var column = new DataGridViewComboBoxColumn();
+                    column.HeaderText = "Acknowledge";
+                    column.DataPropertyName = "Acknowledge";
+                    column.DataSource = new List<string>() { "yes", "no", "unknown" };
+                    dataGridView1.DataSource = table;
+                    dataGridView1.Columns.Add(column);
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                    {
+                        dataGridView1.Rows[i].Cells[3].Value = dataGridView1.Rows[i].Cells[2].Value;
+                    }
+                    dataGridView1.Columns[0].ReadOnly = true;
+                    dataGridView1.Columns[1].ReadOnly = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Table not initilized");
+            }
+            finally
+            {
+
+                con.Close();
+            }
+        }
+
+        private void reloadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            reload();
+        }
+
+        private void openDBLocationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenExplorer(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+        }
+        private static void OpenExplorer(string path)
+        {
+            if (Directory.Exists(path))
+                Process.Start("explorer.exe", path);
         }
     }
 }
